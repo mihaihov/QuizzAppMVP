d8502735aa0cdb1f673f4b8b2418bec85e6a16c7
mihairaducu <raducumihaicristian@gmail.com>
2024-07-01 15:29:31 +0300

    Dependencies and assets hot fix

--- src/assets/images.tsx
+++ src/assets/images.tsx
@@ -1,11 +1,11 @@
 const images = {
     homepage: {
-        bg1: "./../media/assets/pics/homepage/bg1.jpg",
-        newArrow: "./../media/assets/pics/homepage/newArrow.svg",
-        book: "./../media/assets/pics/homepage/book.svg",
-        quiz2: "./../media/assets/pics/homepage/quiz2.jpg",
-        bgMih: "./../media/assets/pics/homepage/bgMih.jpg",
-        logoNew: "./../media/assets/pics/homepage/logoNew.svg"
+        bg1: './bg1.jpg',
+        newArrow: "./newArrow.svg",
+        book: "./book.svg",
+        quiz2: "./quiz2.jpg",
+        bgMih: "./bgMih.jpg",
+        logoNew: "./logoNew.svg"
     }
 };
 
